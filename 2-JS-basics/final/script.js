0b1c650bcbf080cb73bc8902adf35b2810fd3ba0
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2020-06-11 13:21:20 +0200

    Variable Mutation and Type coercion

--- 2-JS-basics/final/script.js
+++ 2-JS-basics/final/script.js
@@ -1,11 +1,62 @@
-var firstName = "Mero";
+/* 
+    Variables and data types
+*/
 
-console.log(firstName);
+// var firstName = "Mero";
 
-var lastName = "Hegazi";
+// console.log(firstName);
 
-var age = 26;
+// var lastName = "Hegazi";
 
-var job = "developer";
+// var age = 26;
 
-console.log(job);
+// var job = "developer";
+
+// console.log(job);
+
+/* 
+    Variable Mutation and type coercion
+*/
+
+// var firstName = "John";
+// var age = 23;
+
+// console.log(firstName + " " + age);
+
+// var job, isMarried;
+// job = "teacher";
+// isMarried = false;
+
+// console.log(
+//   firstName +
+//     " is a " +
+//     age +
+//     " year old " +
+//     job +
+//     " Is he married? " +
+//     isMarried
+// );
+
+// /*
+//     Variable Mutation ( change the value of a variable )
+// */
+
+// age = "twenty eight";
+// job = "driver";
+
+// alert(
+//   firstName +
+//     " is a " +
+//     age +
+//     " year old " +
+//     job +
+//     " Is he married? " +
+//     isMarried
+// );
+
+// var lastName = prompt("What is his last Name");
+// console.log(firstName + " " + lastName);
+
+/*
+    Basic Operators
+*/
