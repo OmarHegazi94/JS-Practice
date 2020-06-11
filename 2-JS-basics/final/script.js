e650f3378c17c3138633cdcc07c5c82f571cfb01
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2020-06-11 13:31:59 +0200

    finished basic math + logical operators

--- 2-JS-basics/final/script.js
+++ 2-JS-basics/final/script.js
@@ -60,3 +60,28 @@
 /*
     Basic Operators
 */
+
+var year, yearJohn, yearMark;
+now = 2020;
+ageJohn = 28;
+ageMark = 33;
+// Math Operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+// Logical Operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof "Mark is older the John");
+var x;
+console.log(typeof x);
