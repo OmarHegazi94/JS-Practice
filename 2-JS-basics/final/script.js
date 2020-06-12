f76d3bc95279efe44d7f3d6a11b37a69bd7f1b23
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2020-06-12 12:19:21 +0200

    Objects and methods

--- 2-JS-basics/final/script.js
+++ 2-JS-basics/final/script.js
@@ -476,30 +476,49 @@ GOOD LUCK 😀
  */
 
 //  Object literal
+// var john = {
+//   firstName: "John",
+//   lastName: "Smith",
+//   birthYear: 1990,
+//   family: ["Jane", "Mark", "Bob", "Emily"],
+//   job: "teacher",
+//   isMarried: false,
+// };
+
+// console.log(john.firstName);
+// console.log(john["lastName"]);
+
+// var x = "birthYear";
+// console.log(john[x]);
+
+// john.job = "designer";
+// john["isMarried"] = true;
+
+// console.log(john);
+
+// // new Object syntax
+// var jane = new Object();
+// jane.firstName = "Jane";
+// jane.birthYear = 1969;
+// jane["lastName"] = "Smith";
+
+// console.log(jane);
+
+/*****************************
+ * Objects and methods
+ */
+
 var john = {
   firstName: "John",
   lastName: "Smith",
-  birthYear: 1990,
+  birthYear: 1989,
   family: ["Jane", "Mark", "Bob", "Emily"],
   job: "teacher",
   isMarried: false,
+  calcAge: function () {
+    this.age = 2020 - this.birthYear;
+  },
 };
 
-console.log(john.firstName);
-console.log(john["lastName"]);
-
-var x = "birthYear";
-console.log(john[x]);
-
-john.job = "designer";
-john["isMarried"] = true;
-
+john.calcAge();
 console.log(john);
-
-// new Object syntax
-var jane = new Object();
-jane.firstName = "Jane";
-jane.birthYear = 1969;
-jane["lastName"] = "Smith";
-
-console.log(jane);
